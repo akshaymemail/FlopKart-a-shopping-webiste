bdeb6ce651aa957587ab5b0d4d79467b8c9be36d
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2021-03-31 14:04:26 +0530

    revert update profile action

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
 // constants
-const PORT = process.env.PORT || 5000
+const PORT = 5000 || process.env.PORT
 
 // connecting to the mongodb
-mongoose.connect(process.env.MONGODB_URI||process.env.DB_STRING,{
+mongoose.connect(process.env.DB_STRING,{
     useNewUrlParser : true,
     useUnifiedTopology : true,
     useCreateIndex : true
@@ -28,21 +28,13 @@ app.use('/api/users', userRouter)
 app.use('/api/products', productRouter)
 app.use('/api/orders', orderRouter)
 
-// app.get('/', (req, res) =>{
-//     res.send("Node is here!")
-// })
+app.get('/', (req, res) =>{
+    res.send("Node is here!")
+})
 
 app.get('/api/payment/method/paypal',isAuth, (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID)
 })
-
-// check if app is on production
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname,"../frontend/build", "index.html"))
-    })
-}
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`)
